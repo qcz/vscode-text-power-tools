4071b551bdc48522497c93e59dd6f0a4200dc410
Dániel Tar <bdanee88@gmail.com>
2018-12-22 08:04:39 +0100

    Add additional output formats for the Generate GUID command

--- src/modules/generateGuid.ts
+++ src/modules/generateGuid.ts
@@ -13,6 +13,11 @@ interface IGenerateGuidOptions {
 	count: "single" | "multiple";
 }
 
+const FORMAT_NO_DASHES = "No dashes";
+const FORMAT_DASHES = "Dashes";
+const FORMAT_DASHES_AND_BRACES = "Dashes and braces";
+const FORMAT_CSHARP = "C# Guid contructor";
+
 export async function generateGuid(options: IGenerateGuidOptions) {
 	const editor = vscode.window.activeTextEditor;
 	if (!editor) {
@@ -20,14 +25,44 @@ export async function generateGuid(options: IGenerateGuidOptions) {
 		return;
 	}
 
-	if (options.count === "multiple") {
-		askForGuidCount(editor);
-	} else {
-		generateGuidInternal(editor, 1);
-	}
+	askForGuidFormat(editor, options);
 }
 
-export async function askForGuidCount(editor: vscode.TextEditor) {
+export async function askForGuidFormat(editor: vscode.TextEditor, options: IGenerateGuidOptions) {
+	const settings = getExtensionSettings();
+	let sampleGuid = v4();
+	if (settings.insertUppercaseGuids) {
+		sampleGuid = sampleGuid.toUpperCase();
+	}
+	
+	const qp = vscode.window.createQuickPick();
+	qp.items = [
+		{ label: FORMAT_NO_DASHES, description: sampleGuid.replace(/[-]/g, "") },
+		{ label: FORMAT_DASHES, description: sampleGuid },
+		{ label: FORMAT_DASHES_AND_BRACES, description: `{${sampleGuid}}` },
+		{ label: FORMAT_CSHARP, description: `new Guid("${sampleGuid}");` }
+	];
+
+	qp.onDidAccept(() => {
+		if (!qp.activeItems.length) {
+			return;
+		}
+		
+		const selectedType = qp.activeItems[0].label;
+
+		if (options.count === "multiple") {
+			askForGuidCount(editor, selectedType);
+		} else {
+			generateGuidInternal(editor, selectedType, 1);
+		}
+
+		qp.hide();
+		qp.dispose();
+	});
+	qp.show();
+}
+
+export async function askForGuidCount(editor: vscode.TextEditor, type: string) {
 	vscode.window.showInputBox({
 		placeHolder: `Please enter how many GUIDs do you want to generate`,
 		value: "1",
@@ -47,11 +82,13 @@ export async function askForGuidCount(editor: vscode.TextEditor) {
 			return;
 		}
 
-		generateGuidInternal(editor, guidCount);
+		generateGuidInternal(editor, type, guidCount);
 	});
 }
 
-export async function generateGuidInternal(editor: vscode.TextEditor, guidCount: number) {
+
+
+export async function generateGuidInternal(editor: vscode.TextEditor, type: string, guidCount: number) {
 	const settings = getExtensionSettings();
 	const replacesBySelection: string[][] = [];
 	const selections = getPureSelections(editor);
@@ -60,9 +97,21 @@ export async function generateGuidInternal(editor: vscode.TextEditor, guidCount:
 		replacesBySelection.push([]);
 
 		for (let i = 0; i < guidCount; i++) {
-			var guid = v4();
-			replacesBySelection[replacesBySelection.length - 1].push(
-				settings.insertUppercaseGuids ? guid.toUpperCase() : guid);
+			let guid = v4();
+			
+			if (settings.insertUppercaseGuids) {
+				guid = guid.toUpperCase();
+			}
+
+			if (type === FORMAT_NO_DASHES) {
+				guid = guid.replace(/[-]/g, "");
+			} else if (type === FORMAT_DASHES_AND_BRACES) {
+				guid = `{${guid}}`;
+			} else if (type === FORMAT_CSHARP) {
+				guid = `new Guid("${guid}");`;
+			}
+
+			replacesBySelection[replacesBySelection.length - 1].push(guid);
 		}
 	}
 
