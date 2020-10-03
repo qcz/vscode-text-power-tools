18e3adaf205ced4043192666b73f3b6123ad011f
Unknown <bdanee88@gmail.com>
2020-10-03 10:35:30 +0200

    Update readme, bump version to 1.16.0

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
-export const NO_ACTIVE_EDITOR: string = "This function requires an active editor. "
-	+ "If you have an open editor, the file may be too large, and VS Code hides it from Text Power Tools.";
+export const NO_ACTIVE_EDITOR: string = "This function requires an active editor.\n\n"
+	+ "If you have an open editor, the file may be too large. VS Code hides these files from extensions like *Text Power Tools*. Vote on [this issue](https://github.com/Microsoft/vscode/issues/31078) to make this limit lifted.";
