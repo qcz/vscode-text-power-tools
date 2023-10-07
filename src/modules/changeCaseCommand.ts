389c53f2a7b5ca913203c63e8c4237e427ad87cb
Dániel Tar <bdanee88@gmail.com>
2023-10-07 14:03:20 +0200

    Handle unicode letters properly in change case commands

--- src/modules/changeCaseCommand.ts
+++ src/modules/changeCaseCommand.ts
@@ -1,5 +1,5 @@
 import * as cc from "change-case";
-import { noCase, pathCase, sentenceCase } from "change-case";
+import { sentenceCase } from "change-case";
 import { spongeCase } from "sponge-case";
 import { swapCase } from "swap-case";
 import { titleCase } from "title-case";
@@ -28,6 +28,14 @@ interface IChangeCaseOptions {
 	type: ChangeCaseType;
 }
 
+const CHANGE_CASE_OPTIONS: cc.Options = {
+	splitRegexp: [
+		/(\p{Ll}|\p{Lo}|\p{N})(\p{Lu}|\p{Lt})/gu, // one lower case char or digit followed by one upper case char
+		/(\p{Lu}|\p{Lt})((?:\p{Lu}|\p{Lt})(?:\p{Ll}|\p{Lo}))/gu // one upper case char followed by one upper case char and then by one lower case char
+	],
+	stripRegexp: /[^\p{L}\d]+/giu
+};
+
 export async function runChangeCaseCommand(options: IChangeCaseOptions) {
 	const editor = vscode.window.activeTextEditor;
 	if (!editor) {
@@ -48,19 +56,19 @@ export async function runChangeCaseCommand(options: IChangeCaseOptions) {
 					currentSelectionLines.push(camelCase(lineContent));
 					break;
 				case ChangeCaseType.PascalCase:
-					currentSelectionLines.push(cc.pascalCase(lineContent));
+					currentSelectionLines.push(cc.pascalCase(lineContent, CHANGE_CASE_OPTIONS));
 					break;
 				case ChangeCaseType.SnakeCase:
-					currentSelectionLines.push(cc.snakeCase(lineContent));
+					currentSelectionLines.push(cc.snakeCase(lineContent, CHANGE_CASE_OPTIONS));
 					break;
 				case ChangeCaseType.DashCase:
-					currentSelectionLines.push(cc.paramCase(lineContent));
+					currentSelectionLines.push(cc.paramCase(lineContent, CHANGE_CASE_OPTIONS));
 					break;
 				case ChangeCaseType.ConstantCase:
-					currentSelectionLines.push(cc.constantCase(lineContent));
+					currentSelectionLines.push(cc.constantCase(lineContent, CHANGE_CASE_OPTIONS));
 					break;
 				case ChangeCaseType.DotCase:
-					currentSelectionLines.push(cc.dotCase(lineContent));
+					currentSelectionLines.push(cc.dotCase(lineContent, CHANGE_CASE_OPTIONS));
 					break;
 				case ChangeCaseType.SwapCase:
 					currentSelectionLines.push(swapCase(lineContent));
@@ -72,22 +80,25 @@ export async function runChangeCaseCommand(options: IChangeCaseOptions) {
 					currentSelectionLines.push(spongeCase(lineContent));
 					break;
 				case ChangeCaseType.SentenceCase:
-					currentSelectionLines.push(sentenceCase(lineContent));
+					currentSelectionLines.push(sentenceCase(lineContent, CHANGE_CASE_OPTIONS,));
 					break;
 				case ChangeCaseType.SeparateWithSpaces:
-					currentSelectionLines.push(noCase(lineContent, {
-						transform: (input: string) => input
+					currentSelectionLines.push(cc.noCase(lineContent, {
+						...CHANGE_CASE_OPTIONS,
+						transform: (input: string) => input,
 					}));
 					break;
 				case ChangeCaseType.SeparateWithForwardSlashes:
-					currentSelectionLines.push(pathCase(lineContent, {
-						transform: (input: string) => input
+					currentSelectionLines.push(cc.pathCase(lineContent, {
+						...CHANGE_CASE_OPTIONS,
+						transform: (input: string) => input,
 					}));
 					break;
 				case ChangeCaseType.SeparateWithBackslashes:
-					currentSelectionLines.push(pathCase(lineContent, {
+					currentSelectionLines.push(cc.pathCase(lineContent, {
+						...CHANGE_CASE_OPTIONS,
 						delimiter: "\\",
-						transform: (input: string) => input
+						transform: (input: string) => input,
 					}));
 					break;
 				default:
